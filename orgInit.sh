f90ca89450602bdf857c6fc0e119c844218c699a
mehboob <77236545+zeb0x01@users.noreply.github.com>
2023-12-19 16:13:16 +0530

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,9 @@
-# sfdx force:apex:log:list --target-org hub 
-# sfdx config:list --target-org hub 
-# sfdx force:apex:log:get --target-org hub 
-# sfdx config:get --target-org hub
-# sfdx force:config:list --configfile /app/.sf/config.json 
-sfdx force:mdapi:retrieve -o hub
+# # sfdx force:apex:log:list --target-org hub 
+# # sfdx config:list --target-org hub 
+# # sfdx force:apex:log:get --target-org hub 
+# # sfdx config:get --target-org hub
+# # sfdx force:config:list --configfile /app/.sf/config.json 
+# sfdx force:mdapi:retrieve -o hub
+
+
+sfdx auth:list -o hub
