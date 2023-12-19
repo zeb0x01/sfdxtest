2898c94fe016c0fc7beaa282d224156fc10fb50d
mehboob <77236545+zeb0x01@users.noreply.github.com>
2023-12-19 15:21:49 +0530

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,2 +1,4 @@
 sfdx force:org:list
-sfdx force:user:list
+sfdx force:user:list --target-org hub
+sfdx force:user:list --target-org 00D6A000001V3UBUA0
+sfdx force:user:list --target-org deploybot@dxhub.org
