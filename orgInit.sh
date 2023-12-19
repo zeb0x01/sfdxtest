0dc2654341ad155ca7ed4b7e37b16a0f547fce4c
mehboob <77236545+zeb0x01@users.noreply.github.com>
2023-12-19 17:26:06 +0530

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -4,9 +4,11 @@
 # # sfdx config:get --target-org hub
 # # sfdx force:config:list --configfile /app/.sf/config.json 
 # sfdx force:mdapi:retrieve -o hub
-sfdx org display --target-org hub --verbose
+# sfdx org display --target-org hub --verbose
 # sfdx auth:list
 # sfdx force:org:list
 # sfdx force:user:list --target-org hub
 # sfdx force:user:list --target-org deploybot@dxhub.org
-sfdx config:get --target-org "00D6A000001V3UB!AQEAQCwSBTl7n5Jn8qYPov.yplEbbWJJv6l_NhOBQqjIhBu0gsQjxSvSXyMYMOqSqp4NalIljtUzIAv5xbOMn632wo11DWAs"
+# sfdx config:get --target-org "00D6A000001V3UB!AQEAQCwSBTl7n5Jn8qYPov.yplEbbWJJv6l_NhOBQqjIhBu0gsQjxSvSXyMYMOqSqp4NalIljtUzIAv5xbOMn632wo11DWAs"
+
+sfdx env list --all
